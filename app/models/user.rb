69d20d97cd498cd7c2b1036bfe7a8a7d2f42e71d
Umer Zahid <proturbo111@gmail.com>
2026-06-30 16:35:30 +0500

    feat: add authentication foundation and user roles

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum :role, {
+    admin: 0,
+    manager: 1,
+    member: 2
+  }
+end
